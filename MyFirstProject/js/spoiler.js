43562c5b152c456d9a056bb437a002bb181a9995
SAI <solovei.alex.iv@gmail.com>
2021-06-18 22:17:08 +0300

    v01

---
+++ MyFirstProject/js/spoiler.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+   $('.spoiler__text').hide();
+   $('.spoiler__title').click(function(event){
+      if($('.spoiler').hasClass('spoiler-one')){
+         $('.spoiler__title').not($(this)).removeClass('spoiler-active');
+         $('.spoiler__text').not($(this).next()).slideUp(300);
+      }
+      $(this).toggleClass('spoiler-active').next().slideToggle(300);
+
+   });
+});
